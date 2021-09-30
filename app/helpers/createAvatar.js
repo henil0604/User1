75ab5e54189e5f8394b6896288fe7a5ad128e3f0
henil0604 <henilmalaviya06@gmail.com>
2021-09-30 19:53:12 +0530

    fixed arguments

--- app/helpers/createAvatar.js
+++ app/helpers/createAvatar.js
@@ -1,9 +1,9 @@
-const sprite = "bottts";
-const baseUri = `https://avatars.dicebear.com/api/${sprite}`;
+const helperJs = require("@henil0604/helperjs");
 
-module.exports = (username) => {
 
-    const seed = `${username}-${Date.now()}`;
+module.exports = (seed = helperJs.random(15), sprite = "bottts") => {
+    // LINK https://avatars.dicebear.com
+    const baseUri = `https://avatars.dicebear.com/api/${sprite}`;
 
     const uri = `${baseUri}/${seed}.svg`;
 
