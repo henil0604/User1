263041ebdcc0472e8439b25bc51d747d2b2f8a20
henil0604 <henilmalaviya06@gmail.com>
2021-09-30 19:55:08 +0530

    Allows to send email

---
+++ app/helpers/sendEmail.js
@@ -0,0 +1,58 @@
+const nodemailer = require("nodemailer");
+
+module.exports = async (data = {}) => {
+
+    // create reusable transporter object using the default SMTP transport
+    let transporterOptions = {
+        host: env("NODEMAILER_host"),
+        port: env("NODEMAILER_port") || 465,
+        secure: env("NODEMAILER_secure") || true,
+        auth: {
+            user: env("NODEMAILER_user"),
+            pass: env("NODEMAILER_password"),
+        }
+    }
+
+    let testAccount = null;
+
+    if (env("NODEMAILER_testAccount")) {
+        testAccount = await nodemailer.createTestAccount();
+        transporterOptions = {
+            ...transporterOptions,
+            ...testAccount.smtp
+        };
+        transporterOptions.auth = {
+            user: testAccount.user,
+            pass: testAccount.pass
+        }
+    }
+
+    let transporter = nodemailer.createTransport(transporterOptions);
+
+    let mailOptions = {
+        from: transporterOptions.auth.user,
+        ...data
+    }
+
+    if (env("NODEMAILER_testAccount")) {
+        mailOptions.from = testAccount.user;
+    }
+
+    let info = null;
+
+    try {
+        info = await transporter.sendMail(mailOptions);
+    } catch (e) {
+        console.log(e)
+        return {
+            status: "error",
+            message: "Failed to send Mail",
+            e: e
+        }
+    }
+
+    return {
+        status: "success",
+        info
+    };
+}
