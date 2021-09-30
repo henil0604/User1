5f458d6608ec1be14825f5104e434edced887a00
henil0604 <henilmalaviya06@gmail.com>
2021-09-30 19:48:35 +0530

    implemented Initialize_MongoDb and using requestIp middleware

---
+++ server.js
@@ -0,0 +1,35 @@
+const log = require("./app/helpers/log");
+log("Loading Modules...");
+let app = require('./app');
+const env = require("./app/helpers/env");
+const RouterManager = require("./app/middlewares/RouterManager")
+const PORT = env("PORT") || 4141;
+const path = require("path");
+const requestIp = require('request-ip');
+
+// Initilizing MongoDb Connection
+require("./app/helpers/Initialize_MongoDb")()
+
+app.use(requestIp.mw())
+app.use(require("./app/middlewares/hit"));
+app.use(require("./app/middlewares/RequestParser"));
+
+// Setting Globals
+globalThis.env = env;
+globalThis.app = app;
+globalThis.log = log;
+globalThis.imp = (d) => {
+    const p = `${path.join(__dirname, d)}`
+    return require(p);
+}
+
+app = RouterManager(
+    app,
+    require("./app/data/routes")
+)
+
+
+app.listen(PORT, () => {
+    log(`Listening on PORT {${PORT}}`, 'success');
+    log(`Load time: {${Date.now() - app.__STARTING_TIME}ms}`);
+});
