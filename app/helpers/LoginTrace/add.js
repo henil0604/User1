5f175f173554859ab157bfb1e42df5bdad94e2fc
henil0604 <henilmalaviya06@gmail.com>
2021-10-02 11:55:42 +0530

    add login trace to specific user

---
+++ app/helpers/LoginTrace/add.js
@@ -0,0 +1,25 @@
+const getLoginTrace = require("./get");
+const $LoginTrace = imp("app/Models/LoginTrace");
+
+
+module.exports = async (userId, trace) => {
+
+    const loginTrace = await getLoginTrace({
+        userId
+    })
+
+    if (!loginTrace) return null
+
+    loginTrace.traces.set(trace.traceId, trace);
+
+    await $LoginTrace.findOneAndUpdate({
+        userId,
+        $set: {
+            traces: loginTrace.traces
+        }
+    })
+
+    return getLoginTrace({
+        userId
+    })
+}
