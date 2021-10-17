e3d4b6e9887fe995a93226e30b856a17b221b7a2
henil0604 <henilmalaviya06@gmail.com>
2021-10-17 14:10:19 +0530

    fixed Function

--- app/helpers/LoginTrace/get.js
+++ app/helpers/LoginTrace/get.js
@@ -1,17 +1,11 @@
 const $LoginTrace = imp("app/Models/LoginTrace");
 
 module.exports = async (find) => {
-    let loginTraces = await $LoginTrace.find(find);
+    let loginTraces = await $LoginTrace.findOne(find);
 
     if (!loginTraces) {
         return null;
     }
-
-    if (loginTraces.length == 0) {
-        return null;
-    }
-
-    loginTraces = loginTraces.length == 1 ? loginTraces[0] : loginTraces;
 
     return loginTraces;
 }
